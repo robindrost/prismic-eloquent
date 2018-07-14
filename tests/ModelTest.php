085c8c9f9133e993f6f5f0d2a599cea9f8c1cca7
Robin Drost <robin.drost@oneshoe.nl>
2018-07-14 21:29:53 +0200

    PHPCS PSR2 fixes.

--- tests/ModelTest.php
+++ tests/ModelTest.php
@@ -11,72 +11,72 @@ class ModelTest extends \PHPUnit\Framework\TestCase
     /**
      * @var \stdClass
      */
-     protected $document;
+    protected $document;
 
-     public function setUp()
-     {
-         $this->document = json_decode(json_encode([
-             'id' => 1,
-             'data' => [
-                 'title' => 'Test',
-             ],
-         ]));
+    public function setUp()
+    {
+        $this->document = json_decode(json_encode([
+            'id' => 1,
+            'data' => [
+                'title' => 'Test',
+            ],
+        ]));
 
-         return parent::setUp();
-     }
+        return parent::setUp();
+    }
 
-     /**
-      * @test
-      */
-     public function it_can_attach_data()
-     {
-         $model = new ModelStub;
-         $model->attachDocument($this->document);
-         $this->assertEquals($this->document, $model->document);
-     }
+    /**
+     * @test
+    */
+    public function itCanAttachData()
+    {
+        $model = new ModelStub;
+        $model->attachDocument($this->document);
+        $this->assertEquals($this->document, $model->document);
+    }
 
-     /**
-      * @test
-      */
-     public function it_can_access_data_by_properties()
-     {
-         $model = new ModelStub;
-         $model->attachDocument($this->document);
-         $this->assertEquals($this->document->data->title, $model->title);
-     }
+    /**
+     * @test
+    */
+    public function itCanAccessDataByProperties()
+    {
+        $model = new ModelStub;
+        $model->attachDocument($this->document);
+        $this->assertEquals($this->document->data->title, $model->title);
+    }
 
-     /**
-      * @test
-      */
-     public function it_will_use_get_methods_when_specified()
-     {
-         $model = new ModelStubWithGetMethod;
-         $model->attachDocument($this->document);
-         $this->assertEquals($this->document->data->title . ' extra text', $model->title);
-     }
+    /**
+     * @test
+    */
+    public function itWillUseAGetMethodWhenSpecified()
+    {
+        $model = new ModelStubWithGetMethod;
+        $model->attachDocument($this->document);
+        $this->assertEquals($this->document->data->title . ' extra text', $model->title);
+    }
 
     /**
      * @test
      */
-     public function it_can_retrieve_document_attribute()
-     {
-         $model = new ModelStub;
-         $model->attachDocument($this->document);
-         $this->assertEquals($this->document->id, $model->id);
-     }
+    public function itCanRetrieveDocumentAttributes()
+    {
+        $model = new ModelStub;
+        $model->attachDocument($this->document);
+        $this->assertEquals($this->document->id, $model->id);
+    }
 
-     /**
-      * @test
-      */
-     public function it_can_create_an_collection_of_models()
-     {
-         $models = [
-             new ModelStub,
-             new ModelStub,
-             new ModelStub,
-         ];
+    /**
+     * @test
+    */
+    public function itCanCreateACollectionOfModels()
+    {
+        $models = [
+            new ModelStub,
+            new ModelStub,
+            new ModelStub,
+        ];
 
-         $model = new ModelStub;
-         $this->assertInstanceOf(Collection::class, $model->newCollection($models));
-     }
+        $model = new ModelStub;
+        $this->assertInstanceOf(Collection::class, $model->newCollection($models));
+    }
 }
